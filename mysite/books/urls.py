2764eaee22411940e0d50e52c8d8717eeaecd27c
PatAug <pataug91@gmail.com>
2021-06-01 14:34:27 +0200

    add register panel

--- mysite/books/urls.py
+++ mysite/books/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.start_page),
-    path('index/', views.all_books, name = 'index')
+    path('', views.start_page, name = "home"),
+    path('index/', views.all_books, name = 'index'),
+    path("register/", views.register, name="register"),
 ]
--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, authenticate, login
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import BookList
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def start_page(request):
     return render(
@@ -20,3 +22,27 @@ def all_books(request):
         'books/all_books.html',
         {'all_books': lists}
     )
+
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New account created {username}")
+            login(request, user)
+            return redirect(start_page)
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+            return render(request=request,
+                          template_name="main/register.html",
+                          context={"form": form})
+
+    form = UserCreationForm
+    return render(request = request,
+                  template_name = "books/register.html",
+                  context={"form": form})
